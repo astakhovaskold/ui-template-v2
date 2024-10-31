b3b244673d6cb891bbf43b4bffde5bea867497fc
Askold Astakhov <astakhovaskold@gmail.com>
2024-10-31 20:21:08 +1000

    server configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,46 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import react from '@vitejs/plugin-react';
+import path from 'path';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import {ConfigEnv, defineConfig, loadEnv} from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig(({mode}: ConfigEnv) => {
-  const env = {...process.env, ...loadEnv(mode, process.cwd())};
+export default ({mode}: ConfigEnv) => {
+    const env = loadEnv(mode, process.cwd(), '');
 
-  const version = env.npm_package_version;
-  const BUILD_DATE = Date.now();
-  const APP_NAME = env.npm_package_name;
+    const BUILD_DATE = Date.now();
+    const VERSION = env.npm_package_version;
+    const APP_NAME = env.npm_package_name;
 
-  return {
-    define: {
-      _VERSION: `"${version}"`,
-      _BUILD_DATE: `"${BUILD_DATE}"`,
-      _SYSTEM: `"${APP_NAME}"`,
-      _UNIQUE_STATE: env.DEV ? '"development"' : `"${version}_${BUILD_DATE}"`,
-    },
-    plugins: [react()],
-    server: {
-      port: +env.PORT || +env.VITE_PORT || 3000,
-      proxy: {
-        '/api/v1': {
-          target: 'https://api.escuelajs.co/api/v1',
-          changeOrigin: true,
-          secure: false,
-          rewrite: path => path.replace('/api/v1', ''),
+    return defineConfig({
+        define: {
+            __VERSION__: `"${VERSION}"`,
+            __BUILD_DATE__: `"${BUILD_DATE}"`,
+            __SYSTEM__: `"${APP_NAME}"`,
+            __UNIQUE_STATE__: env.DEV ? '"development"' : `"${VERSION}_${BUILD_DATE}"`,
         },
-      }
-    },
-    build: {
-      outDir: './build',
-      minify: true,
-      reportCompressedSize: true,
-    },
-  };
-});
+        plugins: [react()],
+        server: {
+            port: env.PORT ? Number(env.PORT) : 3000,
+            proxy: {
+                '/api/v1': {
+                    target: env.API_URL,
+                    changeOrigin: true,
+                    secure: false,
+                    rewrite: path => path.replace('/api/v1', ''),
+                },
+            },
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'src'),
+                '@/fonts': path.resolve(__dirname, 'public', 'fonts'),
+            },
+        },
+        build: {
+            outDir: './build',
+            minify: true,
+            reportCompressedSize: true,
+        },
+    });
+};
